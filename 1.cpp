d9f11104f9449911c202d2b9498bd5f2328a6b66
hariom38 <rajpoothariom900@gmail.com>
2026-06-03 22:51:48 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"Input size of array"<<endl; 
     int n;
     cin>>n;
-    vector<int>arr(n);
+    vector<int>arr(n); 
     cout<<"Input array element"<<endl;
     for(int i=0;i<n;i++){
         cin>>arr[i];
