6bc579f21e8ee4ce8e26c518351e7736c26c2429
hariom38 <rajpoothariom900@gmail.com>
2026-07-26 21:06:56 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -7,7 +7,7 @@ bool contin_nearby_dublicate(vector<int>& arr, int k){
     int n=arr.size();
     for(int i=0;i<n;i++){
         
-        if(mp.find(arr[i]) !=mp.end() && i-mp[arr[i]]<=k){
+        if(mp.find(arr[i]) !=mp.end() && i-mp[arr[i]]<=k){ 
             return true;
         }
         mp[arr[i]]=i;
