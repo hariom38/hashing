d1374ac9bc2ab16d773265be5f4ed82dcb662449
hariom38 <rajpoothariom900@gmail.com>
2026-07-26 21:07:11 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -6,7 +6,7 @@ bool contin_nearby_dublicate(vector<int>& arr, int k){
     unordered_map<int,int>mp; 
     int n=arr.size();
     for(int i=0;i<n;i++){
-        
+         
         if(mp.find(arr[i]) !=mp.end() && i-mp[arr[i]]<=k){ 
             return true;
         }
